2e93dd9105e689727a2c6c5deaec84234eee41eb
Edionay Aguiar <12487723+edionay@users.noreply.github.com>
2024-01-01 17:47:02 -0300

    chore: split course modules into folders

---
+++ 3-app-with-vue-files/components/my-other-component.js
@@ -0,0 +1,10 @@
+import { ref } from 'vue'
+
+export default {
+    setup() {
+        const count = ref(0)
+
+        return { count }
+    },
+    template: `<div>The other count is {{ count }}</div>`
+}
